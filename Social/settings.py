3fd64e6e2aed5a00ac461cfd39966dbb269c752c
Gururaj S Savalgi <89399330+gururajsavalgi@users.noreply.github.com>
2024-06-10 17:06:14 +0530

    changed

--- Social/settings.py
+++ Social/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6_0kvr73wmtfdjwq%wad!luueo6sjc_z#hs^!9r#6(d75&-&*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
